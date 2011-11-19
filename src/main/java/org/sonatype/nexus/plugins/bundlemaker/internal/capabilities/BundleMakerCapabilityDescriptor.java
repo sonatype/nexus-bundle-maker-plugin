1e4eec490ca4eb792143a4cfe05bf9493dff0f12
Alin Dreghiciu <adreghiciu@gmail.com>
2011-11-19 17:13:52 +0200

    Use the new abstract capability descriptor available in Nexus 1.10
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- src/main/java/org/sonatype/nexus/plugins/bundlemaker/internal/capabilities/BundleMakerCapabilityDescriptor.java
+++ src/main/java/org/sonatype/nexus/plugins/bundlemaker/internal/capabilities/BundleMakerCapabilityDescriptor.java
@@ -18,19 +18,18 @@
  */
 package org.sonatype.nexus.plugins.bundlemaker.internal.capabilities;
 
-import java.util.Arrays;
-import java.util.List;
-
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.nexus.formfields.FormField;
 import org.sonatype.nexus.formfields.RepoOrGroupComboFormField;
+import org.sonatype.nexus.plugins.capabilities.api.descriptor.AbstractCapabilityDescriptor;
 import org.sonatype.nexus.plugins.capabilities.api.descriptor.CapabilityDescriptor;
 
 @Singleton
 @Named( BundleMakerCapability.ID )
 public class BundleMakerCapabilityDescriptor
+    extends AbstractCapabilityDescriptor
     implements CapabilityDescriptor
 {
 
@@ -38,44 +37,16 @@ public class BundleMakerCapabilityDescriptor
 
     public static final String REPO_OR_GROUP_ID = "repoOrGroup";
 
-    private final FormField repoOrGroup;
-
-    private final FormField remoteRepositories;
-
-    private final FormField eager;
-
-    private final FormField useMavenModel;
-
     public BundleMakerCapabilityDescriptor()
     {
-        repoOrGroup = new RepoOrGroupComboFormField( REPO_OR_GROUP_ID, FormField.MANDATORY );
-        eager = new EagerFormField();
-        useMavenModel = new UseMavenModelFormField();
-        remoteRepositories = new RemoteRepositoriesFormField();
-    }
-
-    @Override
-    public String id()
-    {
-        return ID;
-    }
-
-    @Override
-    public String name()
-    {
-        return "Bundle Maker capability";
-    }
-
-    @Override
-    public List<FormField> formFields()
-    {
-        return Arrays.asList( repoOrGroup, eager, useMavenModel, remoteRepositories );
-    }
-
-    @Override
-    public boolean isExposed()
-    {
-        return true;
+        super(
+            ID,
+            "Bundle Maker capability",
+            new RepoOrGroupComboFormField( REPO_OR_GROUP_ID, FormField.MANDATORY ),
+            new EagerFormField(),
+            new UseMavenModelFormField(),
+            new RemoteRepositoriesFormField()
+        );
     }
 
 }
